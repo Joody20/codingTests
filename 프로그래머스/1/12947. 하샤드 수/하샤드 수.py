45235dbb88e2286a03808555c815adc86db51fad
dazeroro <127274575+Joody20@users.noreply.github.com>
2026-08-20 14:27:42 +0900

    [level 1] Title: 하샤드 수, Time: 0.01 ms, Memory: 11.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.py
@@ -0,0 +1,13 @@
+def solution(x):
+    answer = True
+    n = 0
+    
+    for i in str(x):
+        n += int(i)
+    
+    if x % n == 0:
+        answer = True
+    else:
+        answer = False
+        
+    return answer
