2c4e46586730b1bfeb268154f15c18c48ccc73f8
dazeroro <127274575+Joody20@users.noreply.github.com>
2026-09-11 23:18:21 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,14 +1,13 @@
 def solution(n, lost, reserve):
-    # 여벌 체육복이 있는 학생이 체육복을 도난당한 경우에는 빌려줄 수 없어 이걸 처리해줘야돼.
-    lost_set = set(lost) - set(reserve)
-    reserve_set = set(reserve) - set(lost)
-
+    중복없는lost = set(lost) - set(reserve)
+    중복없는reverse = set(reserve) - set(lost)
     
-    for r in sorted(reserve_set):  # 혹시 모를 정렬을 해줬어야 했음.
-        if (r - 1) in lost_set:
-            lost_set.remove(r-1)
-        elif (r + 1) in lost_set:
-            lost_set.remove(r+1)
-            
-    return n - len(lost_set)
+    for r in sorted(중복없는reverse):
+        if (r-1) in 중복없는lost:
+            중복없는lost.remove(r-1)
+        elif (r+1) in 중복없는lost:
+            중복없는lost.remove(r+1)
+    
+    
+    return n - len(중복없는lost)
             
